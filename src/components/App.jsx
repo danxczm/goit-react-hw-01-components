26d5b181af0419e2bdaa6c2518a10215ac577a6e
dan <81313913+danxczm@users.noreply.github.com>
2022-12-28 14:24:13 +0200

    done with markup of Profile element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+/* 
+Створений репозиторій goit-react-hw-01-components.
+Компоненти всіх завдань рендеряться на одній сторінці, всередині загального контейнера - кореневого компонента <App>.
+При здачі домашньої роботи є посилання на репозиторій з вихідним кодом проекту.
+У шапці репозиторія є посилання на живу сторінку на GitHub pages.
+Під час відвідування робочої сторінки (GitHub pages) завдання, в консолі відсутні помилки і попередження.
+Для кожного компонента є окрема папка з файлом React-компонента та файлом стилів.
+Для всіх компонентів описані propTypes.
+Все, що компонент очікує у вигляді пропсів, передається йому під час виклику.
+Імена компонентів зрозумілі та описові.
+JS-код чистий та зрозумілий, використовується Prettier.
+Стилізація виконана CSS-модулями або Styled Components, тому класи в результуючому DOM відрізнятимуться від прикладів.
+Достатньо базової стилізації застосунку, насамперед він повинен працювати, а вже потім бути гарним. Приділяй 20% часу на CSS і 80% на JS.
+*/
+
+import { Profile } from './Profile/Profile';
+import user from 'data/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
     </div>
   );
 };
